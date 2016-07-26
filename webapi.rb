95e6d1ccd2e0cc70caf2a53c80931ce655e120d3
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-07-26 09:07:20 -0700

    Refactor with Lambdas

--- webapi.rb
+++ webapi.rb
@@ -10,14 +10,36 @@ users = {
 }
 
 get '/users' do
-  case accepted_media_type
-  when 'json'
-    content_type 'application/json'
-    users.map {|name, data| data.merge(id: name)}.to_json
-  when 'xml'
-    content_type 'application/xml'
-    Gyoku.xml(users: users)
+  send_data({
+    json: -> { users.map {|name, data| data.merge(id: name)} },
+    xml: -> { {users: users} }
+  })
+end
+
+helpers do
+
+  def send_data(data)
+    case accepted_media_type
+    when 'json'
+      content_type 'application/json'
+      data[:json].call.to_json if data[:json]
+    when 'xml'
+      content_type 'application/xml'
+      Gyoku.xml(data[:xml].call) if data[:xml]
+    end
   end
+
+  def accepted_media_type
+    return 'json' unless request.accept.any? # request.accept is an array of Sinatra::Request::AcceptEntry
+
+    request.accept.each do |mt|
+      return 'json' if %(application/json application/* */*).include?(mt.to_s)
+      return 'xml' if mt.to_s == 'application/xml'
+    end
+
+    halt 406, "Not Acceptable"
+  end
+
 end
 
 get '/' do
@@ -27,15 +49,4 @@ end
 get '/debug' do
   content_type 'application/json'
   request.accept.to_json
-end
-
-def accepted_media_type
-  return 'json' unless request.accept.any? # request.accept is an array of Sinatra::Request::AcceptEntry
-
-  request.accept.each do |mt|
-    return 'json' if %(application/json application/* */*).include?(mt.to_s)
-    return 'xml' if mt.to_s == 'application/xml'
-  end
-
-  halt 406, "Not Acceptable"
 end
